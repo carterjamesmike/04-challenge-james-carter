b73caa79e1a42fbf3a134ea07ad2e81d8f898d01
James Carter <carterjamesmike@gmail.com>
2022-10-07 15:25:43 -0500

    Basic Javascript functions added

--- assests/js/script.js
+++ assests/js/script.js
@@ -8,10 +8,17 @@ var optionA = document.createElement("button");
 var optionB = document.createElement("button");
 var optionC = document.createElement("button");
 var optionD = document.createElement("button");
+var startButton = document.createElement("button"); 
 
-score.textContent = "This is the score:"
+var question1 = ["Which button is A?", "A", "B", "C", "D"];
+//
+//
+//
+
+score.textContent = "Press button to start game:"
 timer.textContent = "This is the timer:"
 questions.textContent = "This is the question";
+startButton.textContent = "Start";
 optionA.textContent = "Option A";
 optionB.textContent = "Option B";
 optionC.textContent = "Option C";
@@ -21,8 +28,33 @@ body.appendChild(header);
 header.appendChild(score);
 header.appendChild(timer);
 body.appendChild(questions);
+body.appendChild(startButton);
 body.appendChild(answers);
-answers.appendChild(optionA);
-answers.appendChild(optionB);
-answers.appendChild(optionC);
-answers.appendChild(optionD);
+
+function startQuiz () {
+    startButton.remove();
+    questions.textContent = question1[0];
+    answers.appendChild(optionA);
+    optionA.textContent = question1[1];
+    answers.appendChild(optionB);
+    optionB.textContent = question1[2];
+    answers.appendChild(optionC);
+    optionC.textContent = question1[3];
+    answers.appendChild(optionD);
+    optionD.textContent = question1[4];
+
+    optionA.addEventListener("click", correct);
+    optionB.addEventListener("click", wrong);
+    optionC.addEventListener("click", wrong);
+    optionD.addEventListener("click", wrong);
+}
+
+
+ function correct () {
+    questions.textContent = "Boom!";
+ }
+
+ function wrong () {
+    questions.textContent = "Dang!";
+ }
+startButton.addEventListener("click", startQuiz)
